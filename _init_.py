cb6894fd96ad67224615157e66ba0234c5ea3248
Zerensky <pure.evil97@mail.ru>
2023-07-03 11:22:36 +0600

    init

---
+++ _init_.py
@@ -0,0 +1,4 @@
+"""
+Соберите пакет с играми из тех файлов, что уже были созданы в рамках курса
+"""
+__all__ = ['next_functions', 'multy', 'two_numbers', 'two_numbers_two', 'deco_param', 'deco_sum_func', 'main']
